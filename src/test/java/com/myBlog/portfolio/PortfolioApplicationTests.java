629318713a89da28060687dbf9b9b376ea06a53e
U-hyeon <jeon-uhyeon@jeon-uhyeons-MacBook-Pro.local>
2024-11-19 01:18:08 +0900

    initial commit demo

---
+++ src/test/java/com/myBlog/portfolio/PortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myBlog.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
